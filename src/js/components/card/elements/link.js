adb67a88ce702e13ff2cdaea401c7fbcc8eebc36
Hamza Erbay <hamza@udemy.com>
2019-02-10 13:08:27 -0800

    No need to display block for that.
    That was causing the false click.

--- src/js/components/card/elements/link.js
+++ src/js/components/card/elements/link.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 const Paragraph = styled.a`
     color: ${props => props.theme.colorWhite};
     font-size: 1.8vmin;
-    display: block;
     opacity: 0.7;
     transition: opacity 0.3s ease-in-out;
     text-decoration: none;
